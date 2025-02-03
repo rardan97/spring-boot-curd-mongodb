cad63e24d2e0bd6138258bdac4f23cf590e2eefb
rname97 <risalardan@gmail.com>
2025-02-03 08:30:22 +0700

    add project

---
+++ src/main/java/com/blackcode/spring_boot_curd_mongodb/SpringBootCurdMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.blackcode.spring_boot_curd_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCurdMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCurdMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackcode/spring_boot_curd_mongodb/SpringBootCurdMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackcode.spring_boot_curd_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCurdMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
